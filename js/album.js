3ba064972367824bf17f3b54f3a2dd4a700ebc5a
Nikita Baskov <nb500205@gmail.com>
2023-02-17 17:44:39 +0700

    Добавил progress bar и время трека

--- js/album.js
+++ js/album.js
@@ -1,22 +1,13 @@
 let search = new URLSearchParams(window.location.search);
 let id = search.get(`i`);
-console.log(id);
 
-let playButton = document.querySelectorAll(`.play-button`);
+albumRender();
+tracksRender();
+setupAudio();
 
-for (let i = 0; i < playButton.length; i++){
-  playButton[i].addEventListener(`click`,function(){
-    if (playButton[i].classList.contains('is-play')){
-      playButton[i].src = 'assets/play_icon.png';
-      playButton[i].classList.remove('is-play');
-    } else {
-      playButton[i].src = 'assets/pause_icon.png';
-      playButton[i].classList.add('is-play');
-    }
-  });
-}
 
-document.querySelector(`.album`).innerHTML = `
+function albumRender(){
+  document.querySelector(`.album`).innerHTML = `
   <div class="row row-cols-1 row-cols-xl-2">
     <div class="col col">
       <img src="${albums[id].img}" class="img-fluid rounded-start album-img" >
@@ -33,17 +24,77 @@ document.querySelector(`.album`).innerHTML = `
       </div>
     </div>
   </div>
-`;
-
-for (let i = 0; i < albums[id].tracks.length; i++){
-  document.querySelector(`.playlist`).innerHTML += `
-  <li class="list-group-item d-flex align-item-center">
-    <img src="assets/play_icon.png" class="me-3 play-button" height="38">
-    <div>
-      <div>${albums[id].tracks[id].title}</div>
-      <div class="text-muted">${albums[id].tracks[i].author}</div>
-    </div>
-    <div class="ms-auto">${albums[id].tracks[i].time}</div>
-</li>
   `;
+}
+
+function tracksRender(){
+  for (let i = 0; i < albums[id].tracks.length; i++){
+    document.querySelector(`.playlist`).innerHTML += `
+    <li class="list-group-item d-flex align-item-center track">
+      <img src="assets/play_icon.png" class="me-3 play-button" height="38">
+      <div class="title">
+        <div>${albums[id].tracks[i].title}</div>
+        <div class="text-muted">${albums[id].tracks[i].author}</div>
+      </div>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="0" style="width: 0%" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <div class="ms-auto time">${albums[id].tracks[i].time}</div>
+      <audio class="audio" src="${albums[id].tracks[i].src}"></audio>
+  </li>
+    `;
+  }
+}
+
+
+function setupAudio() {
+  let tracks = albums[id].tracks;
+  let trackNodes = document.querySelectorAll(`.track`);
+  let playButtons = document.querySelectorAll(`.play-button`);
+
+  for (let i = 0; i < trackNodes.length; i++) { 
+    
+    let node = trackNodes[i];
+    let timeNode = node.querySelector(`.time`);
+    let audio = node.querySelector(`.audio`);
+    let progressBar = node.querySelector(`.progress-bar`);
+    
+    node.addEventListener('click',function(){
+      if (tracks[i].isPlaying) {
+        tracks[i].isPlaying = false;
+        audio.pause();
+        playButtons[i].src = `assets/play_icon.png`;
+      } else {
+        tracks[i].isPlaying = true;
+        audio.play();
+        updateProgress();
+        playButtons[i].src = `assets/pause_icon.png`;
+      }
+    });
+    
+    function updateProgress(){
+      time = getTime(audio.currentTime);
+      if (time.innerHTML != time) {
+        timeNode.innerHTML = time;
+        progressBar.style.width = audio.currentTime * 100 / audio.duration + '%';
+      }
+      if (tracks[i].isPlaying) {
+        requestAnimationFrame(updateProgress);
+      }    
+    }
+  }
+}
+
+function getTime(time){
+  let currentSecinds = Math.floor(time);
+  let minutes = Math.floor(currentSecinds/60);
+  let seconds = Math.floor(currentSecinds%60);
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds
+  }
+
+  return `${minutes}:${seconds}`
 }
--- js/data.js
+++ js/data.js
@@ -1,131 +1,171 @@
 let albums = [
   {
-    title: 'The Eminem Show',
-    img: 'assets/The Eminem Show.png',
-    description: 'The Eminem Show — четвёртый студийный альбом американского рэпера Эминема и его первый сольный альбом, изданный на собственном лейбле Shady Records в 2002 году. Создавая этот альбом, Эминем пытался избавиться от скандалов, которые преследовали его в начале карьеры.',
-    year: '26 мая 2002 г.',
+    title: `The Eminem Show`,
+    img: `assets/img/The Eminem Show.png`,
+    description: `The Eminem Show — четвёртый студийный альбом американского рэпера Эминема и его первый сольный альбом, изданный на собственном лейбле Shady Records в 2002 году. Создавая этот альбом, Эминем пытался избавиться от скандалов, которые преследовали его в начале карьеры.`,
+    year: `26 мая 2002 г.`,
     tracks: [
       {
-        title: 'Curtains Up <span class="text-muted">SKIT</span>',
-        author: 'Eminem',
-        time: '0:30'
+        title: `Curtains Up <span class="text-muted">SKIT</span>`,
+        author: `Eminem`,
+        time: `0:30`,
+        src: `assets/music/Eminem - Curtains Up (Skit).mp3`,
+        isPlaying: false
       },
       {
-        title: 'White America',
-        author: 'EminemJeff, BassLuis, Resto',
-        time: '5:24'
+        title: `White America`,
+        author: `EminemJeff, BassLuis, Resto`,
+        time: `5:24`,
+        src: `assets/music/Eminem - White America.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Business',
-        author: 'Dr. Dre',
-        time: '4:11'
+        title: `Business`,
+        author: `Dr. Dre`,
+        time: `4:11`,
+        src: `assets/music/Eminem - Business.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Cleanin` Out My Closet',
-        author: 'Eminem, Bass',
-        time: '4:57'
+        title: `Cleanin' Out My Closet`,
+        author: `Eminem, Bass`,
+        time: `4:57`,
+        src: `assets/music/Eminem - Cleanin' Out My Closet.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Square Dance',
-        author: 'Eminem, Bass',
-        time: '5:23'
+        title: `Square Dance`,
+        author: `Eminem, Bass`,
+        time: `5:23`,
+        src: `assets/music/Eminem - Square Dance.mp3`,
+        isPlaying: false
       },
       {
-        title: 'The Kiss <span class="text-muted">SKIT</span>',
-        author: 'Eminem',
-        time: '1:15'
+        title: `The Kiss <span class="text-muted">SKIT</span>`,
+        author: `Eminem`,
+        time: `1:15`,
+        src: `assets/music/Eminem - The Kiss (Skit).mp3`,
+        isPlaying: false
       },
       {
-        title: 'Soldier',
-        author: 'Eminem',
-        time: '3:46'
+        title: `Soldier`,
+        author: `Eminem`,
+        time: `3:46`,
+        src: `assets/music/Eminem - Soldier.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Say Goodbye Hollywood',
-        author: 'Eminem',
-        time: '4:32'
+        title: `Say Goodbye Hollywood`,
+        author: `Eminem`,
+        time: `4:32`,
+        src: `assets/music/Eminem - Say Goodbye Hollywood.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Drips',
-        author: 'Eminem, Bass',
-        time: '4:45'
+        title: `Drips`,
+        author: `Eminem, Bass`,
+        time: `4:45`,
+        src: `assets/music/Eminem,Obie Trice - Drips.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Without Me',
-        author: 'Eminem, Bass',
-        time: '4:50'
+        title: `Without Me`,
+        author: `Eminem, Bass`,
+        time: `4:50`,
+        src: `assets/music/Eminem - Without Me.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Paul Rosenberg <span class="text-muted">SKIT</span>',
-        author: 'Eminem, Dr. Dre',
-        time: '0:22'
+        title: `Paul Rosenberg <span class="text-muted">SKIT</span>`,
+        author: `Eminem, Dr. Dre`,
+        time: `0:22`,
+        src: `assets/music/Eminem - Paul Rosenberg (Skit).mp3`,
+        isPlaying: false
       },
       {
-        title: 'Sing for the Moment',
-        author: 'Eminem, Bass',
-        time: '5:39'
+        title: `Sing for the Moment`,
+        author: `Eminem, Bass`,
+        time: `5:39`,
+        src: `assets/music/Eminem - Sing For The Moment.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Superman',
-        author: 'Eminem, Bass',
-        time: '5:50'
+        title: `Superman`,
+        author: `Eminem, Bass`,
+        time: `5:50`,
+        src: `assets/music/Eminem,Dina Rae - Superman.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Hailie`s Song',
-        author: 'Eminem',
-        time: '5:20'
+        title: `Hailie's Song`,
+        author: `Eminem`,
+        time: `5:20`,
+        src: `assets/music/Eminem - Hailie's Song.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Steve Berman <span class="text-muted">SKIT</span>',
-        author: 'Dr. Dre',
-        time: '0:33'
+        title: `Steve Berman <span class="text-muted">SKIT</span>`,
+        author: `Dr. Dre`,
+        time: `0:33`,
+        src: `assets/music/Eminem - Steve Berman (Skit).mp3`,
+        isPlaying: false
       },
       {
-        title: 'When the Music Stops',
-        author: 'Eminem, Mr. Porter',
-        time: '4:29'
+        title: `When the Music Stops`,
+        author: `Eminem, Mr. Porter`,
+        time: `4:29`,
+        src: `assets/music/Eminem,D12 - When The Music Stops.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Say What You Say',
-        author: 'Dr. Dre',
-        time: '5:09'
+        title: `Say What You Say`,
+        author: `Dr. Dre`,
+        time: `5:09`,
+        src: `assets/music/Eminem,Dr. Dre - Say What You Say.mp3`,
+        isPlaying: false
       },
       {
-        title: '`Till I Collapse',
-        author: 'Eminem',
-        time: '4:57'
+        title: `'Till I Collapse`,
+        author: `Eminem`,
+        time: `4:57`,
+        src: `assets/music/Eminem,Nate Dogg - 'Till I Collapse.mp3`,
+        isPlaying: false
       },
       {
-        title: 'My Dad`s Gone Crazy',
-        author: 'Dr. Dre',
-        time: '4:27'
+        title: `My Dad's Gone Crazy`,
+        author: `Dr. Dre`,
+        time: `4:27`,
+        src: `assets/music/Eminem,Hailie Jade - My Dad's Gone Crazy.mp3`,
+        isPlaying: false
       },
       {
-        title: 'Curtains Close <span class="text-muted">SKIT</span>',
-        author: 'Eminem',
-        time: '1:01'
+        title: `Curtains Close <span class="text-muted">SKIT</span>`,
+        author: `Eminem`,
+        time: `1:01`,
+        src: `assets/music/Eminem - Curtains Close.mp3`,
+        isPlaying: false
       }
     ]
   },
 
   {
-    title: 'Kamikaze',
-    img: 'assets/Kamikaze.png',
-    description: 'Kamikaze — десятый студийный альбом американского рэпера Эминема, выпущенный 31 августа 2018 года без какого-либо предварительного анонса. Продюсерами альбома выступили Dr. Dre и сам Эминем.',
-    year: '31 августа 2018 г.'
+    title: `Kamikaze`,
+    img: `assets/img/Kamikaze.png`,
+    description: `Kamikaze — десятый студийный альбом американского рэпера Эминема, выпущенный 31 августа 2018 года без какого-либо предварительного анонса. Продюсерами альбома выступили Dr. Dre и сам Эминем.`,
+    year: `31 августа 2018 г.`
   },
 
   {
-    title: 'Last One Standing',
-    img: 'assets/Last One Standing.png',
-    description: 'Сингл',
-    year: '2021 г.'
+    title: `Last One Standing`,
+    img: `assets/img/Last One Standing.png`,
+    description: `Сингл`,
+    year: `2021 г.`
   },
 
   {
-    title: 'Revival',
-    img: 'assets/Revival.png',
-    description: 'Revival — девятый альбом американского рэпера Эминема, выпущенный 15 декабря 2017 года на лейблах Aftermath Entertainment, Shady Records и Interscope Records.',
-    year: '15 декабря 2017 г.'
+    title: `Revival`,
+    img: `assets/img/Revival.png`,
+    description: `Revival — девятый альбом американского рэпера Эминема, выпущенный 15 декабря 2017 года на лейблах Aftermath Entertainment, Shady Records и Interscope Records.`,
+    year: `15 декабря 2017 г.`
   },
 ]
